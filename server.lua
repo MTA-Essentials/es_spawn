a0e1a187894b96bb7c4bb0bd166bb9e05535727d
Deihim007 <deihim007@gmail.com>
2020-10-01 22:25:25 +0330

    Add lastLocation logic

--- server.lua
+++ server.lua
@@ -4,15 +4,72 @@ addEventHandler(
     root,
     function()
         local player = client
-        if config.spawn == 'static' then
-            spawn = config.static
-        elseif config.spawn == 'random' then
-        elseif config.spawn == 'last' then
+        local pos = getElementData(player, 'lastLocation')
+        if pos then
+            pos = fromJSON(pos)
+        else
+            pos = {x = 0, y = 0, z = 4, r = 0, i = 0, d = 0}
         end
-        spawnPlayer(player, spawn.x, spawn.y, spawn.z, spawn.r)
+        spawnPlayer(player, pos.x, pos.y, pos.z, pos.r)
         setCameraTarget(player, player)
-        setElementInterior(player, spawn.i)
-        setElementDimension(player, spawn.d)
+        setElementInterior(player, pos.i)
+        setElementDimension(player, pos.d)
+        setTimer(
+            function()
+                fadeCamera(player, true)
+            end,
+            2100,
+            1
+        )
+    end
+)
+
+addEventHandler(
+    'onPlayerQuit',
+    root,
+    function()
+        local player = source
+        local px, py, pz = getElementPosition(player)
+        local rx, ry, rz = getElementRotation(player)
+        local pi, pd = getElementInterior(player), getElementDimension(player)
+        setElementData(player, 'lastLocation', toJSON({x = px, y = py, z = pz, r = rz, i = pi, d = pd}))
+    end
+)
+
+addEventHandler(
+    'onPlayerWasted',
+    root,
+    function()
+        print('huh')
+        local player = source
+        local px, py, pz = getElementPosition(player)
+
+        local result = {}
+        for i, v in ipairs(config.spawns) do
+            result[#result + 1] = getDistanceBetweenPoints3D(px, py, pz, v.x, v.y, v.z)
+        end
+
+        local lowestIndex = 0
+        local lowestValue = false
+        for i, v in ipairs(result) do
+            if not lowestValue or v < lowestValue then
+                lowestIndex = i
+                lowestValue = v
+            end
+        end
+
+        local pos = config.spawns[lowestIndex]
+        fadeCamera(player, false)
+        setTimer(
+            function()
+                spawnPlayer(player, pos.x, pos.y, pos.z, pos.r)
+                setCameraTarget(player, player)
+                setElementInterior(player, pos.i)
+                setElementDimension(player, pos.d)
+            end,
+            1100,
+            1
+        )
         setTimer(
             function()
                 fadeCamera(player, true)
